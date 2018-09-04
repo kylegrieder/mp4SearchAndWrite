8436ad8f8a1cd6fdae48ffb551ef93784dfa9709
Kyle Grieder <kgfromthemv@gmail.com>
2018-09-04 15:34:48 -0600

    Initial Commit

---
+++ mp4SearchAndWrite/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mp4SearchAndWrite
+//
+//  Created by Kyle Grieder on 9/4/18.
+//  Copyright © 2018 Kyle Grieder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
