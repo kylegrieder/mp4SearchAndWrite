5eaa2f4515e681ade6ce2b943a2a88c3c793560b
Kyle Grieder <kyle.grieder@gmail.com>
2018-09-12 23:32:09 -0600

    figured out how to run a script from within swift, finally. still need to import file path to pass to script, handle artwork, and setup async console writes for atomicparsley

--- mp4SearchAndWrite/main.swift
+++ mp4SearchAndWrite/main.swift
@@ -20,24 +20,40 @@ func main() {
         if let title = terms["title"], let year = terms["year"] {
             let movie = Movie(withTitle: title, andYear: year)
             
-            let path = "/bin/sh"
+            // todo handle movie.artwork
             
-            let task = Process()
-            task.launchPath = path
-            task.arguments = ["--login", "cd", "/Users/kylegrieder/Downloads", "ls"]
-            
-            let pipe = Pipe()
-            task.standardOutput = pipe
-            
-            task.launch()
-            
-            let outputData = pipe.fileHandleForReading.readDataToEndOfFile()
-            let outputString = String(data: outputData, encoding: String.Encoding.utf8)
-            
-            consoleIO.writeMessage(outputString!)
+            if let title = movie.title,
+                let genre = movie.genre,
+                let releaseDate = movie.releaseDate,
+                let longDesc = movie.longDesc,
+                let storeDesc = movie.storeDesc,
+                let mpaaCert = movie.mpaaCertification {
+                
+                let arguments = [title, genre, releaseDate, longDesc, storeDesc, mpaaCert, movie.stik]
+                mp4WriteScript(withArguments: arguments)
+            }
         }
-        
     }
+}
+
+func mp4WriteScript(withArguments arguments: [String]) {
+    let path = "/usr/local/bin/mp4Write.sh"
+    
+    let task = Process()
+    task.launchPath = path
+    task.arguments = arguments
+    
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    
+    //            let outputHandle = pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+    
+    task.launch()
+    
+    let outputData = pipe.fileHandleForReading.readDataToEndOfFile()
+    let outputString = String(data: outputData, encoding: String.Encoding.utf8)
+    
+    consoleIO.writeMessage(outputString!)
 }
 
 main()
--- mp4SearchAndWrite/mp4Write.sh
+++ mp4SearchAndWrite/mp4Write.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+# // title, genre, releaseDate, longDesc, storeDesc, mpaaCert, stik
 
+echo ${1}
+echo ${2}
+echo ${3}
+echo ${4}
+echo ${5}
+echo ${6}
+echo ${7}
+
+#/usr/local/bin/atomicparsley "/Users/kylegrieder/Downloads/A Bug's Life.mp4" --title "${1}"
+
+#/usr/local/bin/atomicparsley "/Users/kylegrieder/Downloads/A Bug's Life.mp4" -t
